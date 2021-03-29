04ed0631a515d546541ac528ba0ea8c823e4f43d
SilviaKhShuvechccha <63290771+SilviaKhShuvechccha@users.noreply.github.com>
2021-03-30 00:12:44 +0600

    project added

---
+++ Food Donation/Controllers/LoginSystemController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Food_Donation.Controllers
+{
+    public class LoginSystemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            return View();
+        }
+    }
+}
